5d48fe1f270e9726fa49a68146b1910c78347757
Leonel <leonel_lima19@hotmail.com>
2021-09-14 22:50:00 -0300

    Models for GraphQL

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,8 +1,48 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    type Group {
+        id: ID
+        name: String
+        members: [User]
+    }
+
     type User {
         id: ID
+        name: String
+        email: String
+        username: String
+        position: String
+        telephone: String
+        status: Status
+        imageUrl: String
+    }
+    
+    type Status {
+        emoji: String
+        description: String
+    }
+
+    type Message {
+        id: ID
+        time: String
+        user: User
+        text: String
+        reactions: [String]
+    }
+
+    type Channel {
+        id: ID
+        type: ChannelType
+        name: String
+        public: Boolean
+        members: [User]
+        messages: [Message]
+    }
+
+    enum ChannelType {
+        Channel
+        Direct
     }
 
     type Query {
